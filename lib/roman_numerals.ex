9617d749ec9c72ffa4a48cf6c8762adb48983605
Eddy <eddy147@gmail.com>
2021-03-27 15:27:34 +0100

    test 1

--- lib/roman_numerals.ex
+++ lib/roman_numerals.ex
@@ -4,5 +4,18 @@ defmodule RomanNumerals do
   """
   @spec numeral(pos_integer) :: String.t()
   def numeral(number) do
+    number
+    |> Integer.digits()
+    |> to_roman("")
+  end
+
+  def to_roman([], roman), do: roman
+
+  @spec to_roman(List.t(), String.t()) :: String.t()
+  def to_roman([head | tail], roman) do
+    case head do
+      1 -> to_roman(tail, roman <> "I")
+      _ -> to_roman(tail, roman)
+    end
   end
 end
